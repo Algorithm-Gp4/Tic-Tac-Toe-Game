ab40c07434c00abd8b661e7dc6c86fc30f1431b9
natiman8634 <natiman863400@gmail.com>
2024-11-04 07:18:21 -0800

    Add files via upload
    This is discription of drowboared function

---
+++ Drawb.cpp
@@ -0,0 +1,13 @@
+void drawBoard(char *spaces){
+    cout << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "  " << '\n';
+    cout << "_____|_____|_____" << '\n';
+    cout << "     |     |     " << '\n';
+    cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "  " << '\n';
+    cout << "     |     |     " << '\n';
+    cout << '\n';
+}
